deec8c446b100e7381e5479ebd884cc51e2fb4e7
gallen <gallenliu@hotmail.com>
2017-10-20 16:02:10 -0400

    Find max number 1

--- lab1/find_max.py
+++ lab1/find_max.py
@@ -2,3 +2,10 @@
 
 numbers = [1, -20, 0, 100, -5, 4, 10, 23, -19, 99]
 
+max_num = numbers[0]
+
+for i in numbers:
+    if i > max_num:
+        max_num = i
+
+print("Maximum number in the list ", numbers, "is", max_num )
