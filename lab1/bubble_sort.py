112f67a54f636c4118dda7f0af4b86901a284134
gallen <gallenliu@hotmail.com>
2017-10-20 16:15:22 -0400

    Bubble sort full implement

--- lab1/bubble_sort.py
+++ lab1/bubble_sort.py
@@ -1,2 +1,11 @@
 # Sort list of numbers from least to greatest
 numbers = [5, 4, 3, 1, 2]
+
+for passnum in range(len(numbers) - 1, 0, -1):
+    for i in range(passnum):
+        if numbers[i] > numbers[i + 1]:
+            temp = numbers[i]
+            numbers[i] = numbers[i + 1]
+            numbers[i + 1] = temp
+
+print(numbers)
