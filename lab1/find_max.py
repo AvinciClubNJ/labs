5e64db94e8210b2c04f93765742cfd5578568f00
gallen <gallenliu@hotmail.com>
2017-10-20 16:07:05 -0400

    find max number 3

--- lab1/find_max.py
+++ lab1/find_max.py
@@ -4,7 +4,10 @@ numbers = [1, -20, 0, 100, -5, 4, 10, 23, -19, 99]
 
 max_num = numbers[0]
 
-for i in numbers[1:]:
+iterNumbers = iter(numbers)
+next(iterNumbers)
+
+for i in iterNumbers:
     if i > max_num:
         max_num = i
 
