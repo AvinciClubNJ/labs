40e3a9de2e0e88c0770a9bdc8dd919f90bf26a36
Avinci Club <avinciclubnj@gmail.com>
2017-10-21 00:44:52 +0000

    Triangle sample code

---
+++ python_lesson_sample/HW2/README.md
@@ -0,0 +1,16 @@
+To run program, open a terminal window. Type,
+
+$ ./triangle.py
+
+If the program doesn't run, check python with command "which python". If you have python3 installed, but not named by python3, replace the first line with following.
+
+#!/usr/bin/env python
+
+
+The program take 2 inputs, one for the character to build the triangle, the other one is the triangle base width.
+
+Consider following test cases:
+User doesn't enter anything when asked for an input for character.
+User entered more than one characters.
+User doesn't enter an odd number.
+User entered an invalid number (The program might miss this case, fix it if you can)
---
+++ python_lesson_sample/HW2/triangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+while True:
+    userIn = input("Please enter a char to build the triangle: ")
+    if len(userIn) > 0:
+        c = userIn[0]
+        break
+
+while True:
+    widthIn = input("Please enter the triangle base width, odd number only: ")
+    if len(widthIn) == 0:
+        continue
+
+    width = int(widthIn)
+    if (width % 2 == 0):
+        continue
+    else:
+        break
+
+height = (width + 1) / 2
+
+# Create triangle
+for x in range (0, int(height)):
+    spaceToPrint = " " * int(((width-1)/2) - x)
+    charToPrint = c * int((x*2)+1)
+    print(spaceToPrint, charToPrint)
+
+print()
+
+# Create up side down triangle
+for x in range (int(height), 0, -1):
+    spaceToPrint = " " * int(((width+1)/2) - x)
+    charToPrint = c * int((x*2)-1)
+    print(spaceToPrint, charToPrint)
